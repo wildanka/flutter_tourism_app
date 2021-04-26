6d2ebba525249b5c5d6d1f8767fb497a50373d06
Wildan Kurniadi <danprogramming10@gmail.com>
2021-04-27 02:01:37 +0700

    3 split container into separated widget

--- lib/screens/location_detail/location_detail.dart
+++ lib/screens/location_detail/location_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'text_section.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
@@ -10,18 +11,9 @@ class LocationDetail extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              decoration: BoxDecoration(color: Colors.red),
-              child: Text('1'),
-            ),
-            Container(
-              decoration: BoxDecoration(color: Colors.green),
-              child: Text('2'),
-            ),
-            Container(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text('3'),
-            ),
+            TextSection(),
+            TextSection(),
+            TextSection(),
           ],
         ));
   }
---
+++ lib/screens/location_detail/text_section.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TextSection extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: Colors.red),
+      child: Text('1'),
+    );
+  }
+
+}
